56845c9371e1394ec7a4da655b7ae93a1119b851
denilson <denilson020898@gmail.com>
2023-12-26 15:07:48 +0700

    add

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools_rust import Binding, RustExtension
-from setuptools import setup
 from setuptools import dist
-dist.Distribution().fetch_build_eggs["setuptools_rust"]
+dist.Distribution().fetch_build_eggs(['setuptools_rust'])
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
 
 setup(
     name="denilson_fib_rs",
