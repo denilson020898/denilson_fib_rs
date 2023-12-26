b84142c6e753a602e7698383a951983b012603d8
denilson <denilson020898@gmail.com>
2023-12-26 15:13:41 +0700

    push

--- setup.py
+++ setup.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python
-
 from setuptools import dist
 dist.Distribution().fetch_build_eggs(['setuptools_rust'])
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
+
 setup(
-    name="denilson_fib_rs",
+    name="denilson-fib-rs",
     version="0.1",
     rust_extensions=[RustExtension(
-        "./denilson_fib_rs.denilson_fib_rs",
-        path="Cargo.toml",
-        binding=Binding.PyO3)],
+        ".denilson_fib_rs.denilson_fib_rs",
+        path="Cargo.toml", binding=Binding.PyO3)],
     packages=["denilson_fib_rs"],
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "Programming Language :: Rust",
-        "Operating System :: POSIX",
-        "Operating System :: MacOS :: MacOS X",
-    ],
+            "License :: OSI Approved :: MIT License",
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python",
+            "Programming Language :: Rust",
+            "Operating System :: POSIX",
+            "Operating System :: MacOS :: MacOS X",
+        ],
+    # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
+    # entry_points={
+    #     'console_scripts': [
+    #         'fib-number = denilson_fib_rs.'
+    #         'fib_number_command:'
+    #         'fib_number_command',
+    #         'config-fib = denilson_fib_rs.'
+    #         'config_number_command:'
+    #         'config_number_command',
+    #     ],
+    # },
+    # requirements=[
+    #     "pyyaml>=3.13",
+    #     "numpy"
+    # ]
 )
